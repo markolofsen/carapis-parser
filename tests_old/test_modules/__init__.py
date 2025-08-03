e65ccf2449966832b3370574c54a0a5b159cc830
Mark <markolofsen@gmail.com>
2025-08-03 17:01:13 +0800

    fixes

--- tests_old/test_modules/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Test modules - isolated versions for testing without problematic imports
-"""
-
-# This module provides isolated versions of core modules for testing
-# without the complex import dependencies that break pytest collection 
